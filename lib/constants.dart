ebfd1fbb6833cdd1b41a6260e43bb723bf61ee08
Merhong <zx2938@gmail.com>
2023-10-10 15:04:34 +0900

    쇼핑카트 앱 실습완료

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// 앱 브랜드 색 (흰색)
+const kPrimaryColor = Color(0xFFeeeeee);
+
+// 기본 버튼 색 (회색)
+const kSecondaryColor = Color(0xFFc6c6c6);
+
+// 활성화된 버튼 색 (주황색)
+const kAccentColor = Color(0xFFff7643);
